c9865c02b9f51e12d7ceb3c10fc26b952ffbe3e1
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-18 22:15:12 +0300

    Update

--- src/run/modules/ssmtp/manifests/init.pp
+++ src/run/modules/ssmtp/manifests/init.pp
@@ -1,10 +1,9 @@
 class ssmtp {
-  include ssmtp::packages
+  require ssmtp::packages
 
   file { '/etc/ssmtp/ssmtp.conf':
     ensure => present,
     content => template('ssmtp/ssmtp.conf.erb'),
-    mode => 644,
-    require => Class['ssmtp::packages']
+    mode => 644
   }
 }
