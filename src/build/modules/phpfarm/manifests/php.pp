be377dce4e2dac622be87248176ceb2ea8280648
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-18 15:50:31 +0300

    Update

--- src/build/modules/phpfarm/manifests/php.pp
+++ src/build/modules/phpfarm/manifests/php.pp
@@ -3,14 +3,14 @@ class phpfarm::php {
 
   file { '/phpfarm/src/custom-options-5.5.17-dev.sh':
     ensure => present,
-    source => File['/phpfarm/phpfarm/src/custom-options-5.5.17-dev.sh'],
+    source => File['phpfarm/phpfarm/src/custom-options-5.5.17-dev.sh'],
     mode => 755,
     require => Class['phpfarm']
   }
 
   exec { '/phpfarm/src/compile.sh 5.5.17-dev':
     timeout => 0,
-    require => File['/phpfarm/src/custom-options-5.5.17-dev.sh']
+    require => File['phpfarm/src/custom-options-5.5.17-dev.sh']
   }
 
   exec { 'rm -rf /phpfarm/src/php-5.5.17-dev':
@@ -20,21 +20,21 @@ class phpfarm::php {
 
   file { '/phpfarm/inst/php-5.5.17-dev/etc/php-fpm.conf':
     ensure => present,
-    source => File['/phpfarm/phpfarm/inst/php-5.5.17-dev/etc/php-fpm.conf'],
+    source => File['phpfarm/phpfarm/inst/php-5.5.17-dev/etc/php-fpm.conf'],
     mode => 644,
     require => Exec['/phpfarm/src/compile.sh 5.5.17-dev']
   }
 
   file { '/phpfarm/inst/php-5.5.17-dev/lib/php.ini':
     ensure => present,
-    source => File['/phpfarm/phpfarm/inst/php-5.5.17-dev/lib/php.ini'],
+    source => File['phpfarm/phpfarm/inst/php-5.5.17-dev/lib/php.ini'],
     mode => 644,
     require => Exec['/phpfarm/src/compile.sh 5.5.17-dev']
   }
 
   file { '/etc/profile.d/phpfarm.sh':
     ensure => present,
-    source => File['/phpfarm/etc/profile.d/phpfarm.sh'],
+    source => File['phpfarm/etc/profile.d/phpfarm.sh'],
     mode => 755,
     require => Exec['/phpfarm/src/compile.sh 5.5.17-dev']
   }
