1fec9677ec9e5aa8ea2591ad0d705237d5602a80
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-19 03:01:28 +0300

    Update

--- src/build/modules/php/manifests/extension/zend_debugger.pp
+++ src/build/modules/php/manifests/extension/zend_debugger.pp
@@ -1,8 +1,8 @@
 class php::extension::zend_debugger {
   require php
 
-  file { '/phpfarm/inst/php-5.5.17/lib/php/extensions/ZendDebugger-linux-x86_64/php-5.5.x/ZendDebugger.so':
+  file { '/phpfarm/inst/php-5.5.18/lib/php/extensions/ZendDebugger-linux-x86_64/php-5.5.x/ZendDebugger.so':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.17/lib/php/extensions/ZendDebugger-linux-x86_64/php-5.5.x/ZendDebugger.so'
+    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.18/lib/php/extensions/ZendDebugger-linux-x86_64/php-5.5.x/ZendDebugger.so'
   }
 }
--- src/build/modules/php/manifests/init.pp
+++ src/build/modules/php/manifests/init.pp
@@ -6,45 +6,45 @@ class php {
     path => ['/usr/bin']
   }
 
-  file { '/phpfarm/src/custom-options-5.5.17.sh':
+  file { '/phpfarm/src/custom-options-5.5.18.sh':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/src/custom-options-5.5.17.sh',
+    source => 'puppet:///modules/php/phpfarm/src/custom-options-5.5.18.sh',
     mode => 755,
     require => Exec['git clone git://git.code.sf.net/p/phpfarm/code phpfarm']
   }
 
-  exec { '/phpfarm/src/compile.sh 5.5.17':
+  exec { '/phpfarm/src/compile.sh 5.5.18':
     timeout => 0,
-    require => File['/phpfarm/src/custom-options-5.5.17.sh']
+    require => File['/phpfarm/src/custom-options-5.5.18.sh']
   }
 
-  exec { 'rm -rf /phpfarm/src/php-5.5.17':
+  exec { 'rm -rf /phpfarm/src/php-5.5.18':
     path => ['/bin'],
-    require => Exec['/phpfarm/src/compile.sh 5.5.17']
+    require => Exec['/phpfarm/src/compile.sh 5.5.18']
   }
 
-  file { '/phpfarm/inst/php-5.5.17/etc/php-fpm.conf':
+  file { '/phpfarm/inst/php-5.5.18/etc/php-fpm.conf':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.17/etc/php-fpm.conf',
+    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.18/etc/php-fpm.conf',
     mode => 644,
-    require => Exec['/phpfarm/src/compile.sh 5.5.17']
+    require => Exec['/phpfarm/src/compile.sh 5.5.18']
   }
 
-  file { '/phpfarm/inst/php-5.5.17/lib/php.ini':
+  file { '/phpfarm/inst/php-5.5.18/lib/php.ini':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.17/lib/php.ini',
+    source => 'puppet:///modules/php/phpfarm/inst/php-5.5.18/lib/php.ini',
     mode => 644,
-    require => Exec['/phpfarm/src/compile.sh 5.5.17']
+    require => Exec['/phpfarm/src/compile.sh 5.5.18']
   }
 
   file { '/etc/profile.d/phpfarm.sh':
     ensure => present,
     source => 'puppet:///modules/php/etc/profile.d/phpfarm.sh',
     mode => 755,
-    require => Exec['/phpfarm/src/compile.sh 5.5.17']
+    require => Exec['/phpfarm/src/compile.sh 5.5.18']
   }
 
-  exec { '/bin/bash -l -c "switch-phpfarm 5.5.17"':
+  exec { '/bin/bash -l -c "switch-phpfarm 5.5.18"':
     require => File['/etc/profile.d/phpfarm.sh']
   }
 
