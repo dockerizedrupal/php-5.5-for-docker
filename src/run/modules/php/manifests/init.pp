73c7cefe37cc07fa02cc6d12cddc9677dbc328c5
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-22 23:14:25 +0200

    Update

--- src/run/modules/php/manifests/init.pp
+++ src/run/modules/php/manifests/init.pp
@@ -14,4 +14,9 @@ class php {
   if $redis_host {
     include php::redis
   }
+
+  file { '/phpfarm/inst/php-5.6.1/lib/php.ini':
+    ensure => present,
+    content => template('php/php.ini.erb')
+  }
 }
