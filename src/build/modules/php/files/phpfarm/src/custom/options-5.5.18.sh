66b2c39179168413275e7cf299db30744d0a0252
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-03 11:12:05 +0200

    Update

--- src/build/modules/php/files/phpfarm/src/custom/options-5.5.18.sh
+++ src/build/modules/php/files/phpfarm/src/custom/options-5.5.18.sh
@@ -37,4 +37,5 @@ configoptions="--with-libdir=/lib/x86_64-linux-gnu \
   --with-gettext \
   --with-pspell \
   --with-ldap=/usr \
+  --with-ldap=/usr \--with-config-file-scan-dir=/phpfarm/inst/php-5.5.18/etc/conf.d \
 "
