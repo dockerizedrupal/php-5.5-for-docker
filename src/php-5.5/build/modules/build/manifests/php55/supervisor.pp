188f9cc646036ffc0984fa8966a348f21b2d62a4
Jürgen Viljaste <j.viljaste@gmail.com>
2015-12-20 01:56:44 +0200

    #20

--- src/php-5.5/build/modules/build/manifests/php55/supervisor.pp
+++ src/php-5.5/build/modules/build/manifests/php55/supervisor.pp
@@ -1,7 +1,7 @@
 class build::php55::supervisor {
-  file { '/etc/supervisor/conf.d/php-5.5.conf':
+  file { '/etc/supervisor/conf.d/php.conf':
     ensure => present,
-    source => 'puppet:///modules/build/etc/supervisor/conf.d/php-5.5.conf',
+    source => 'puppet:///modules/build/etc/supervisor/conf.d/php.conf',
     mode => 644
   }
 }
